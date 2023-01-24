0b26da4b3c2a3ae1d40393e9aefe8686588a379b
fluttereasy <Android@easysolution.asia>
2023-01-23 23:32:58 -0800

    first commit

--- lib/Screens/Notification/notification_screen.dart
+++ lib/Screens/Notification/notification_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class NotificationScreen extends StatefulWidget {
   const NotificationScreen({Key? key}) : super(key: key);
@@ -8,55 +9,306 @@ class NotificationScreen extends StatefulWidget {
 }
 
 class _NotificationScreenState extends State<NotificationScreen> {
-  String content = 'A doctor is someone who is experienced and certifexperienced andal and mental health. A doctor is tasked with interacting with patients.';
+  String content =
+      'A doctor is someone who is experienced and certifexperienced andal and mental health. A doctor is tasked with interacting with patients.';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Notification',
-          style: TextStyle(color: Colors.black),
+        appBar: AppBar(
+          title: const Text(
+            'Notification',
+            style: TextStyle(color: Colors.black),
+          ),
+          backgroundColor: Colors.white,
+          elevation: 0,
         ),
-        backgroundColor: Colors.white,
-        elevation: 0,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Column(
-          children: [
-            Expanded(
-              child: Container(
-                height: 300,
-                width: MediaQuery.of(context).size.width,
-                child: ListView.builder(itemBuilder: (context, index) {
-                  return Column(
-                    children: [
-                      ListTile(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 10),
+                  width: MediaQuery.of(context).size.width,
+                  child: ListTile(
+                    leading: Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.green.withOpacity(0.1),
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      child: const Center(
+                          child: FaIcon(
+                        FontAwesomeIcons.calendarPlus,
+                        color: Colors.green,
+                      )),
+                    ),
+                    title: const Text(
+                      'Appointment Success !',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Container(
+                      margin: const EdgeInsets.all(5),
+                      child: const Text(
+                        'Today | 3:56 PM',
+                        style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    trailing: Container(
+                      height: 25,
+                      width: 40,
+                      decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(10.0)),
+                      child: const Center(
+                          child: Text(
+                        'New',
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      )),
+                    ),
+                  )),
+              Container(
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+                  child: Text(
+                    content,
+                    style: const TextStyle(
+                      color: Colors.grey,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.left,
+                  )),
+              const SizedBox(height: 15),
+              Column(
+                children: [
+                  Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 10),
+                      width: MediaQuery.of(context).size.width,
+                      child: ListTile(
                         leading: Container(
-                            height: 50,
-                            width: 50,
-                            decoration: BoxDecoration(
-                                color: Colors.red[100],
-                                borderRadius: BorderRadius.circular(30.0)),
-                            child: const Icon(Icons.calendar_month)),
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(
+                            color: Colors.pink.withOpacity(0.1),
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                          child: const Center(
+                              child: FaIcon(
+                            FontAwesomeIcons.calendarMinus,
+                            color: Colors.red,
+                          )),
+                        ),
                         title: const Text(
                           'Appointment Cancel !',
-                          style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
                         ),
-                        subtitle: const Text('Today | 3:56 PM',style: TextStyle(color: Colors.grey,fontWeight: FontWeight.bold),),
-                      ),
-                      Container(
-                        child: Text(content,style: const TextStyle(fontWeight: FontWeight.bold,color: Colors.grey,),),
-                      )
-                    ],
-                  );
-                }),
+                        subtitle: Container(
+                          margin: const EdgeInsets.all(5),
+                          child: const Text(
+                            'Today | 3:56 PM',
+                            style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        trailing: Container(
+                          height: 25,
+                          width: 40,
+                          decoration: BoxDecoration(
+                              color: Colors.blue,
+                              borderRadius: BorderRadius.circular(10.0)),
+                          child: const Center(
+                              child: Text(
+                            'New',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          )),
+                        ),
+                      )),
+                  Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 0),
+                      child: Text(
+                        content,
+                        style: const TextStyle(
+                          color: Colors.grey,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.left,
+                      ))
+                ],
               ),
-            )
-          ],
-        ),
-      ),
-    );
+              const SizedBox(height: 15),
+              Column(
+                children: [
+                  Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 10),
+                      width: MediaQuery.of(context).size.width,
+                      child: ListTile(
+                        leading: Container(
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(
+                            color: Colors.orange.withOpacity(0.2),
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                          child: const Center(
+                              child: FaIcon(
+                            FontAwesomeIcons.suitcaseMedical,
+                            color: Colors.deepOrange,
+                          )),
+                        ),
+                        title: const Text(
+                          'New Service Available !',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        subtitle: Container(
+                          margin: const EdgeInsets.all(5),
+                          child: const Text(
+                            '14 Dec 2023 | 3:56 PM',
+                            style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      )),
+                  Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 0),
+                      child: Text(
+                        content,
+                        style: const TextStyle(
+                          color: Colors.grey,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.left,
+                      ))
+                ],
+              ),
+              const SizedBox(height: 15),
+              Column(
+                children: [
+                  Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 10),
+                      width: MediaQuery.of(context).size.width,
+                      child: ListTile(
+                        leading: Container(
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(
+                            color: Colors.blue.withOpacity(0.3),
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                          child: const Center(
+                              child: FaIcon(
+                            FontAwesomeIcons.creditCard,
+                            color: Colors.blueAccent,
+                          )),
+                        ),
+                        title: const Text(
+                          'Credit Card Connected !',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        subtitle: Container(
+                          margin: const EdgeInsets.all(5),
+                          child: const Text(
+                            'Today | 3:56 PM',
+                            style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      )),
+                  Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 0),
+                      child: Text(
+                        content,
+                        style: const TextStyle(
+                          color: Colors.grey,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.left,
+                      ))
+                ],
+              ),
+              const SizedBox(height: 15),
+              Column(
+                children: [
+                  Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 10),
+                      width: MediaQuery.of(context).size.width,
+                      child: ListTile(
+                        leading: Container(
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(
+                            color: Colors.green[100],
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                          child: const Center(
+                              child: FaIcon(
+                            FontAwesomeIcons.calendarPlus,
+                            color: Colors.green,
+                          )),
+                        ),
+                        title: const Text(
+                          'Appointment Success !',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        subtitle: Container(
+                          margin: const EdgeInsets.all(5),
+                          child: const Text(
+                            'Today | 3:56 PM',
+                            style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        trailing: Container(
+                          height: 25,
+                          width: 40,
+                          decoration: BoxDecoration(
+                              color: Colors.blue,
+                              borderRadius: BorderRadius.circular(10.0)),
+                          child: const Center(
+                              child: Text(
+                            'New',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          )),
+                        ),
+                      )),
+                  Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 0),
+                      child: Text(
+                        content,
+                        style: const TextStyle(
+                          color: Colors.grey,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.left,
+                      ))
+                ],
+              ),
+            ],
+          ),
+        ));
   }
 }
