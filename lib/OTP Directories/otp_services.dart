2698d718a833e1f4909b1095ed2e44acda093f2b
fluttereasy <Android@easysolution.asia>
2023-02-12 20:32:44 -0800

    otp and profile bloc corrected

---
+++ lib/OTP Directories/otp_services.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:hospital_app/Constant/constant.dart';
+import 'package:http/http.dart' as http;
+
+class OtpServices {
+  static int? getOtp;
+  //Function to Send Otp
+  Future<dynamic> sendOtp(String mobileNumber) async {
+    try {
+      const String endPoint = 'GenrateOTPByNumber?MobileNumber=';
+      var response = await http
+          .post(Uri.parse("${'${ConstantApi.baseUrl}$endPoint'}$mobileNumber"));
+      if (response.statusCode == 200) {
+        var jsondata = jsonDecode(response.body.toString());
+        var otpReceived = (jsondata[0]);
+        OtpServices.getOtp = otpReceived;
+        return otpReceived;
+      }
+    } catch (e) {}
+  }
+}
